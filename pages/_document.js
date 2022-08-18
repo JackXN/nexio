ce6723460ed2bccc184a0d5f9f449dabc7704cf6
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-08-17 18:56:22 -0600

    _document.js +

---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+import Document, {Html, Head, Main, NextScript} from "next/document";
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" />
+<link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Catamaran:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
+{/* AOS */}
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" />
+                </Head>
+                <body>
+                    <Main/>
+                    <NextScript/>
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument;
